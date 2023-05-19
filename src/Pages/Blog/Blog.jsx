a054df7f909ed4e39466c268310f1273b4fe3197
masudra <masudrana6278312@gmail.com>
2023-05-20 00:24:40 +0600

    blog page css add

--- src/Pages/Blog/Blog.jsx
+++ src/Pages/Blog/Blog.jsx
@@ -1,8 +1,29 @@
 
 const Blog = () => {
     return (
-        <div>
-            
+        <div className="p-10">
+            <div className="mt-8">
+                <h3 className="text-2xl"><span className="font-bold text-2xl">Question 1:</span></h3>
+                <p className="text-xl"><span className="font-bold text-xl">Answer 1:</span></p>
+
+            </div>
+            <div  className="mt-8">
+                <h3 className="text-2xl"><span className="font-bold text-2xl">Question 2:</span></h3>
+                <p className="text-xl"><span className="font-bold text-xl">Answer 2:</span></p>
+
+            </div>
+            <div  className="mt-8">
+                <h3 className="text-2xl"><span className="font-bold text-2xl">Question 3:</span></h3>
+                <p className="text-xl"><span className="font-bold text-xl">Answer 3:</span></p>
+
+            </div>
+            <div  className="mt-8">
+                <h3 className="text-2xl"><span className="font-bold text-2xl">Question 4:</span></h3>
+                <p className="text-xl"><span className="font-bold text-xl">Answer 4:</span></p>
+
+            </div>
+
+
         </div>
     );
 };
